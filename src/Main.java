6cb64ffcb91ca485c081c27b9125b096f7b95a50
A-M-98 <110609152+A-M-98@users.noreply.github.com>
2022-10-03 10:51:52 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Random;
+import java.util.Scanner;
+
+//Main is connecting methods from classes.
+
+public class Main {
+    public static void main(String[] args) {
+        PlayHangman.StarGame();
+    }
+}
